47caae3213e7a6a5bd26fa8dfc9a7a7f8e30a2a9
Juan Vargas <vargasm.jp@gmail.com>
2025-06-26 20:57:27 +0200

    Fix: add base to run github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,42 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/portfolio_2025/", 
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    target: "esnext",
+    outDir: "dist",
+    assetsDir: "assets",
+    sourcemap: mode === "development",
+    minify: mode === "production" ? "esbuild" : false,
+    cssCodeSplit: true,
+    chunkSizeWarningLimit: 500,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        },
+      },
+    },
+  },
+  esbuild: {
+    legalComments: "none",
+    drop: mode === "production" ? ["console", "debugger"] : [],
+  },
+  define: {
+    "process.env.NODE_ENV": `"${mode}"`,
   },
 }));
